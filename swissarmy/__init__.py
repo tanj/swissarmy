583026130d41fe3327d87cb10a811302dc215607
John teBokkel <john@powercoreeng.com>
2019-03-08 11:59:57 -0500

    add sig fig rounder

--- swissarmy/__init__.py
+++ swissarmy/__init__.py
@@ -1,4 +1,8 @@
 #swissarmy/__init__.py
+from math import (
+    log,
+    trunc,
+)
 
 def flatten(container):
     """flatten an arbitrarily nested list or tuple"""
@@ -18,3 +22,12 @@ def safe_cast(type, value, safe=None):
         return type(value)
     except:
         return safe
+
+
+def sigfig(val, sigs=3):
+    """Round val to <sigs> significant figures
+
+       return float
+    """
+    nExp = sigs - (1 + trunc(log(abs(float(val)), 10)))
+    return round(float(val) * (10.0**nExp)) / (10.0**nExp)

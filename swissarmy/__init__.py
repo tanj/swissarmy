154ea307016cff7f0cd6fb4ca0f150ea23829fa3
John teBokkel <john@powercoreeng.com>
2020-04-23 17:54:15 -0700

    add EmptyNoneFormatter

--- swissarmy/__init__.py
+++ swissarmy/__init__.py
@@ -1,8 +1,13 @@
-#swissarmy/__init__.py
+# swissarmy/__init__.py
 from math import (
     log,
     trunc,
 )
+
+from .EmptyNoneFormatter import EmptyNoneFormatter
+
+fmtEmpty = EmptyNoneFormatter()
+
 
 def flatten(container):
     """flatten an arbitrarily nested list or tuple"""
@@ -13,6 +18,7 @@ def flatten(container):
         else:
             yield i
 
+
 def safe_cast(type, value, safe=None):
     """try and cast the value to the type
 
@@ -20,7 +26,7 @@ def safe_cast(type, value, safe=None):
     """
     try:
         return type(value)
-    except:
+    except Exception:
         return safe
 
 
@@ -30,4 +36,26 @@ def sigfig(val, sigs=3):
        return float
     """
     nExp = sigs - (1 + trunc(log(abs(float(val)), 10)))
-    return round(float(val) * (10.0**nExp)) / (10.0**nExp)
+    return round(float(val) * (10.0 ** nExp)) / (10.0 ** nExp)
+
+
+def iter_or_none(val):
+    """Return val if iterable else None"""
+    try:
+        if any(True for _ in val):
+            return val
+        return None
+    except Exception:
+        return None
+
+
+def iter_or_list(val):
+    """Return val if iterable else None"""
+    try:
+        if any(True for _ in val):
+            return val
+        return []
+    except Exception:
+        return [
+            val,
+        ]

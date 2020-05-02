53651d062bd395b55fe12d3602b0ae71ffe601eb
John teBokkel <john@powercoreeng.com>
2020-05-01 20:27:39 -0700

    update email

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Swiss Army: Untility Package",
-    version = "0.1",
-    packages = find_packages(),
-
-
-
-    author = "John teBokkel",
-    author_email = "john@powercoreeng.com",
-    description = "A collection of various functions and utilities that keep getting recreated",
+    name="Swiss Army: Untility Package",
+    version="0.1",
+    packages=find_packages(),
+    author="John teBokkel",
+    author_email="tanj.tanj@gmail.com",
+    description="A collection of various functions and utilities that keep getting recreated",
 )

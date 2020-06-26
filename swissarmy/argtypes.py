2f0771504ae527b06debbbc1c569eca6e3d286d9
John teBokkel <john@powercoreeng.com>
2020-06-26 15:59:58 -0700

    argparse valid args

---
+++ swissarmy/argtypes.py
@@ -0,0 +1,27 @@
+import os.path
+from argparse import ArgumentTypeError, ArgumentError
+
+
+def valid_dir(arg):
+    if os.path.isdir(arg):
+        return arg
+    raise ArgumentTypeError(f"{arg!r} is not a valid directory")
+
+
+def valid_file(arg):
+    if os.path.isfile(arg):
+        return arg
+    raise ArgumentTypeError(f"{arg!r} is not a valid file")
+
+
+def valid_connection(string):
+    from sqlalchemy import create_engine
+    from sqlalchemy.exc import DBAPIError
+
+    try:
+        eng = create_engine(string)
+        con = eng.connect()
+    except (ArgumentError, DBAPIError) as e:
+        raise ArgumentTypeError(e)
+    con.close()
+    return string

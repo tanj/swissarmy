22d641ccea018b6d207784deca6f6d2ab5c57a7c
John teBokkel <john@powercoreeng.com>
2020-11-25 19:44:14 -0800

    change package name to follow PEP508 and bump verison

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Swiss Army: Untility Package",
-    version="0.1",
+    name="swissarmy",
+    version="0.1.1",
     packages=find_packages(),
     author="John teBokkel",
     author_email="tanj.tanj@gmail.com",
